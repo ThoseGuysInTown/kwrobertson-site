e5bf40c97261a8d3f74e9c003a4920e9efef0aee
Kevin Robertson <robertson.kevin.w@gmail.com>
2015-09-06 00:11:03 -0700

    Gallery Work
    Finally got the images to display in an adaptable way.  Still don’t
    have the pics for them yet though.

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,11 @@
 var main = function() {
 
+
+	// for Image loading
+	function imgLoaded(img){
+    	var art = img.parentNode;
+    	art.className += art.className ? ' loaded' : 'loaded';
+	};
 };
 
 $(document).ready(main);
