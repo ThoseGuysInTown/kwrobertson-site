991fd11f95c803c6115cde04be414923d5220894
Kevin Robertson <robertson.kevin.w@gmail.com>
2015-09-08 22:10:45 -0700

    About KR
    Working on the about kr page.  Will animate from the top.

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,7 @@
 var main = function() {
 
+	
 
-	// for Image loading
-	function imgLoaded(img){
-    	var art = img.parentNode;
-    	art.className += art.className ? ' loaded' : 'loaded';
-	};
 };
 
 $(document).ready(main);
